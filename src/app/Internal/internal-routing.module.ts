19baea65431249e1d3249d2b4cd523319c813096
Vilmantas Petrusevičius <vilmantas.petrusevicius@gmail.com>
2018-02-19 22:54:54 +0200

    a

--- src/app/Internal/internal-routing.module.ts
+++ src/app/Internal/internal-routing.module.ts
@@ -3,8 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', loadChildren: './account/public.module#PublicModule'},
-  //{ path: 'private', loadChildren: './private/private.module#PrivateModule' },
+  { path: '', loadChildren: './account/public.module#PublicModule'}
 ];
 
 @NgModule({
